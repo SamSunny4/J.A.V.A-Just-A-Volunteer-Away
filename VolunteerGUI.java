68f76a2718a96d24d60337c72f552210702af851
JVN <johnvnettady@gmail.com>
2025-10-22 23:11:47 +0530

    fix: fixed timing

--- VolunteerGUI.java
+++ VolunteerGUI.java
@@ -168,7 +168,7 @@ public class VolunteerGUI extends JFrame {
      */
     private void startSplashTimer() {
         // Use a Timer to transition from splash to login after delay
-        javax.swing.Timer timer = new javax.swing.Timer(6000, e -> {
+        javax.swing.Timer timer = new javax.swing.Timer(9000, e -> {
             cardLayout.show(mainPanel, LOGIN_PANEL);
         });
         timer.setRepeats(false);
